d81da07591f58f55256306e778bf1972596049e4
Tony Petty <tpetty@appmobi.com>
2013-09-03 21:28:01 -0400

    Working.k

--- tpetty.php
+++ tpetty.php
@@ -21,42 +21,14 @@ $params = array(
 $sql = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME like 'tbl_Practice0%';";
 $stmt = sqlsrv_query( $conn, $sql, $params, array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
 $row_count = sqlsrv_num_rows( $stmt );
-echo $row_count;
-if( $stmt === false ) {
-     die( print_r( sqlsrv_errors(), true));
+if ($row_count === FALSE) die('crap');
+
+for (var i in $row_count) {
+	if( sqlsrv_fetch( $stmt ) === false) {
+		die( print_r( sqlsrv_errors(), true));
+	}
+	$name = sqlsrv_get_field( $stmt, 0);
+	echo "$name: ";
 }
-
-if( sqlsrv_fetch( $stmt ) === false) {
-	die( print_r( sqlsrv_errors(), true));
-}
-
-$name = sqlsrv_get_field( $stmt, 0);
-echo "$name: ";
-
-$comment = sqlsrv_get_field( $stmt, 1);
-echo $comment;
 echo "done";
-
-// Initialize parameters and prepare the statement.
-// Variables $qty and $id are bound to the statement, $stmt.
-/*
-$qty = 0; $id = 0;
-$stmt = sqlsrv_prepare( $conn, $sql, array( &$qty, &$id));
-if( !$stmt ) {
-    die( print_r( sqlsrv_errors(), true));
-}
-
-// Set up the SalesOrderDetailID and OrderQty information.
-// This array maps the order ID to order quantity in key=>value pairs.
-$orders = array( 1=>10, 2=>20, 3=>30);
-
-// Execute the statement for each order.
-foreach( $orders as $id => $qty) {
-    // Because $id and $qty are bound to $stmt1, their updated
-    // values are used with each execution of the statement.
-    if( sqlsrv_execute( $stmt ) === false ) {
-          die( print_r( sqlsrv_errors(), true));
-    }
-}
-*/
 ?>
